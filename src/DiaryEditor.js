ed349e4e5a2b514269407a3915af815d100da55a
jeonggu Kang <93317914+jgkang9402@users.noreply.github.com>
2022-04-03 16:01:32 +0900

    useRef

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const DiaryEditor = () =>{ // 다이어리 에디터 컴포넌트 생성
+
+  const authorInput =useRef();
+  const contentInput =useRef();
 
   const [state,setState] = useState({
     author: "",
@@ -18,7 +21,17 @@ const DiaryEditor = () =>{ // 다이어리 에디터 컴포넌트 생성
     })
   };
   const handleSubmit = ()=>{
-    console.log(state);
+    // console.log(state);
+    if(state.author.length < 1){
+      // alert("작성자는 최소 1글자 이상 입력해주세요.")
+      authorInput.current.focus();
+      return;
+    }
+    if(state.content.length < 5){
+      // alert("일기 본문은 최소 5글자 이상 입력해주세요.")
+      contentInput.current.focus();
+      return;
+    }
     alert('저장 성공')
   }
 
@@ -26,10 +39,10 @@ const DiaryEditor = () =>{ // 다이어리 에디터 컴포넌트 생성
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input name="author" value={state.author} onChange={handleChangeState} />
+        <input ref={authorInput} name="author" value={state.author} onChange={handleChangeState} />
       </div>
       <div>
-        <textarea name="content" value={state.content} onChange={handleChangeState} />
+        <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState} />
       </div>
       <div>
           <span>오늘의 감정점수 : </span>
