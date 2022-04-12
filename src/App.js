0068b12055b04f8500fe660fcc5477fc69257015
jeonggu Kang <93317914+jgkang9402@users.noreply.github.com>
2022-04-12 11:09:58 +0900

    CRUD - delete

--- src/App.js
+++ src/App.js
@@ -21,10 +21,17 @@ const App = () => {
     setData([newItem, ...data]); // 원래데이터(...data)에 새로운아이템(newIteam)추가
   }
 
+  const onRemove = (targetId)=>{
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiarylist = data.filter((it) => it.id !== targetId);
+    console.log(newDiarylist);
+    setData(newDiarylist);
+  }
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList diaryList={data} />
+      <DiaryList onRemove={onRemove} diaryList={data} />
     </div>
   );
 };
